4b590dc4743b7709f6ca6bf3be28ada7def8802e
milovanderlinden <milo@dogodigi.net>
2015-03-23 11:32:24 +0100

    Grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,24 @@ module.exports = function (grunt) {
                 }
             }
         },
+        copy: {
+            main: {
+                files: 
+                    [
+                        {
+                            cwd: 'public/images',
+                            src: [ '**' ],
+                            dest: 'build/public/images',
+                            expand: true
+                        }
+                    ]
+            }
+        },
+        clean: {
+            build: {
+                src: ['build']
+            }
+        },
         less: {
             dist: {
                 files: {
@@ -31,9 +49,31 @@ module.exports = function (grunt) {
             }
         }
     });
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask(
+            'build',
+            'Compiles all of the assets and copies the files to the build directory.',
+            ['clean', 'copy']
+            );
     grunt.registerTask('default', ['less', 'cssmin', 'nodeunit']);
+    grunt.registerTask('exportdbk', 'Export all DBK objects to json', function () {
+        global.conf = require('nconf');
+        global.conf.argv().env();
+        global.conf.file({file: 'config.json'});
+
+        // Force task into async mode and grab a handle to the "done" function.
+        var done = this.async();
+        // Run some sync stuff.
+        grunt.log.writeln('Processing task...');
+        // And some async stuff.
+        setTimeout(function () {
+            grunt.log.writeln('All done!');
+            done();
+        }, 1000);
+    });
 };
 
